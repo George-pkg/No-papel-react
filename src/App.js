7aff642474e66ef315ee07c9f8e27e3c498cf0ca
Gerge <67079118+kawazuke@users.noreply.github.com>
2023-09-21 16:37:14 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import './App.css';
-
-import logo from "./img/login.png"
+import logo from "./img/login.png";
+import { useState } from 'react';
 
 function App() {
+  // visualizar o valor, alterar o valor
+  const [valueInput, setValueInput] = useState("");
+
+  const handleClick = (event) => {
+    
+  }
+  const handleChange = (event) => {
+    console.log(event.target.value)
+  }
   return (
     <div className="App">
 
       <main>
         <img src={logo} alt='Sem papel logo'></img>
         <form className="form">
-          <input type="text" placeholder='Name'/>
+          <input type="text" placeholder='Name' onChange={handleChange}/>
           <input type="password" placeholder='Password'/>
-          <button type="summit">login</button>
+          <button type="summit" onClick={handleClick}>login</button>
         </form>
 
         <div className="Dice-pass">
           <label id="save">
-            <input type="checkbox" name="save-password" id="save"/>            
+            <input type="checkbox" name="save-password" id="save"/>
             <p>Remember me</p>
           </label>
+
           <label id="forgot">
             <a href="#">Forgot Password</a>
           </label>
