d9c59e6038b1c9242b23732720b44a990644848b
Gerge <67079118+kawazuke@users.noreply.github.com>
2023-09-21 15:27:50 -0300

    create form 1.0

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import './App.css';
 
+import logo from "./img/login.png"
+
 function App() {
   return (
     <div className="App">
-      
+
       <main>
-        <h2>hello wold</h2>      
+        <img src={logo} alt='Sem papel logo'></img>
         <form className="form">
-          <input type="text" placeholder=''/>
+          <input type="text" placeholder='Name'/>
+          <input type="password" placeholder='Password'/>
+          <button type="summit">login</button>
         </form>
+
+        <div className="Dice-pass">
+          <label id="save">
+            <input type="checkbox" name="save-password" id="save"/>            
+            <p>Remember me</p>
+          </label>
+          <label id="forgot">
+            <a href="#">Forgot Password</a>
+          </label>
+        </div>
       </main>
 
     </div>
