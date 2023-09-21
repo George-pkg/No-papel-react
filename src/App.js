a21822fbf7b3fe771b124c8c80d68bdc20eb7a06
Gerge <67079118+kawazuke@users.noreply.github.com>
2023-09-21 11:50:07 -0300

    .

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <main>
+        <h2>hello wold</h2>      
+        <form className="form">
+          <input type="text" placeholder=''/>
+        </form>
+      </main>
+
+    </div>
+  );
+}
+
+export default App;
