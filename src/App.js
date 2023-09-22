45269ff473418301ad50fd0aee270d787384dd97
Gerge <67079118+kawazuke@users.noreply.github.com>
2023-09-22 10:28:59 -0300

    add event button

--- src/App.js
+++ src/App.js
@@ -1,40 +1,12 @@
-import './App.css';
-import logo from "./img/login.png";
-import { useState } from 'react';
+import './css/App.css';
+
+import FormHome from './pages/FormHome/FormHome.js'
 
 function App() {
-  // visualizar o valor, alterar o valor
-  const [valueInput, setValueInput] = useState("");
-
-  const handleClick = (event) => {
-    
-  }
-  const handleChange = (event) => {
-    console.log(event.target.value)
-  }
+ 
   return (
     <div className="App">
-
-      <main>
-        <img src={logo} alt='Sem papel logo'></img>
-        <form className="form">
-          <input type="text" placeholder='Name' onChange={handleChange}/>
-          <input type="password" placeholder='Password'/>
-          <button type="summit" onClick={handleClick}>login</button>
-        </form>
-
-        <div className="Dice-pass">
-          <label id="save">
-            <input type="checkbox" name="save-password" id="save"/>
-            <p>Remember me</p>
-          </label>
-
-          <label id="forgot">
-            <a href="#">Forgot Password</a>
-          </label>
-        </div>
-      </main>
-
+      <FormHome></FormHome>
     </div>
   );
 }
