d680489eb6760e0721dcd9f6a22d676da6e1aefa
Gerge <67079118+kawazuke@users.noreply.github.com>
2023-09-26 12:41:09 -0300

    upgarde
    projeto test, agora irei investir no flutter.

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,9 +1,33 @@
 import Header from "../../components/layout/Header"
+import styles from "./HomePage.module.css"
 
 export default function HomePage() {
     return (
         <>
-          <Header />  
+          <Header />
+            <div className={styles.CotainerSection}>
+              <section className={styles.MenuLeft}>
+                <div>
+                  <p>1</p>
+                </div>
+
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+                <div>
+                  
+                </div>
+              </section>
+
+              <section className={styles.MenuRigth}>
+                <div>
+
+                </div>
+              </section >
+            </div>
         </>
     )
 };
